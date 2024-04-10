71e1351c83771e0d715b62f3ac1a79ebbca02f04
Andy Watt <andy.watt83@gmail.com>
2024-04-10 22:11:38 +0100

    reorder folders

---
+++ BloggingDemo.Shared/IBlogRepository.cs
@@ -0,0 +1,10 @@
+﻿using BloggingDemo.Shared.Models;
+
+namespace BloggingDemo.Shared;
+
+public interface IBlogRepository
+{
+    Task<List<BlogPost>> GetAllPostsAsync();
+    Task<bool> CreateBlogPost(BlogPost post);
+    Task<BlogPost?> GetPostByIdAsync(int id);
+}
